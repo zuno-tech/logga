dbf8e5450ee7f609fa8b10f9eee0c6a72d8c5867
Lorenzo Tello <ltello@zonedigital.com>
2017-07-14 17:01:44 +0200

    add model creation log

--- lib/logga/active_record.rb
+++ lib/logga/active_record.rb
@@ -4,27 +4,37 @@ module Logga
 
     class_methods do
       def add_log_entries_for(*actions)
-        around_update :log_model_changes if actions.include?(:update)
+        after_create :log_model_creation if actions.include?(:create)
+        after_update :log_model_changes  if actions.include?(:update)
       end
     end
 
+    def log_model_creation
+      body = "#{self.class} created"
+      self.log_entries.create(author_data.merge(body: body))
+    end
+
     def log_model_changes
-      field_changes = changes
-      log_field_changes(field_changes) if yield
+      field_changes = changes.except(:created_at, :updated_at, :log)
+      log_field_changes(field_changes, author)
     end
 
-    def log_field_changes(changes)
-      changes.each { |field, values| log_field_change(field, *values) }
+    def log_field_changes(changes, author)
+      changes.each { |field, values| log_field_change(field, *values, author) }
     end
 
-    def log_field_change(field, old_value, new_value)
-      author_data = Hash(try(:author))
-      self.log_entries.create(
-          body:      "changed #{field} from #{old_value} to #{new_value}",
-          author_id:   author_data[:id],
-          author_tupe: author_data[:type],
-          author_name: author_data[:name]
-      )
+    def log_field_change(field, old_value, new_value, author)
+      body = "changed #{field} from #{old_value} to #{new_value}"
+      self.log_entries.create(author_data.merge(body: body))
+    end
+
+    def author_data
+      data = Hash(author).with_indifferent_access
+      {
+        author_id:   data[:id],
+        author_type: data[:type],
+        author_name: data[:name]
+      }
     end
   end
 end
