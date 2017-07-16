ead13a343001c178770baa58360febd6388d68e9
Lorenzo Tello <ltello@zonedigital.com>
2017-07-16 19:42:23 +0200

    allow log entries for a model to be generated by events on a different model

--- lib/logga/active_record.rb
+++ lib/logga/active_record.rb
@@ -3,37 +3,38 @@ module Logga
     extend ActiveSupport::Concern
 
     class_methods do
-      def add_log_entries_for(*actions)
+      def add_log_entries_for(*actions, to: :self)
         after_create :log_model_creation if actions.include?(:create)
         after_update :log_model_changes  if actions.include?(:update)
+        define_method(:log_receiver) { to == :self ? self : send(to) }
       end
     end
 
     def log_model_creation
       body = "#{self.class} created"
-      self.log_entries.create(author_data.merge(body: body))
+      log_receiver.log_entries.create(author_data.merge(body: body))
     end
 
     def log_model_changes
       field_changes = changes.except(:created_at, :updated_at, :log)
-      log_field_changes(field_changes, author)
+      log_field_changes(field_changes)
     end
 
-    def log_field_changes(changes, author)
-      changes.each { |field, values| log_field_change(field, *values, author) }
+    def log_field_changes(changes)
+      changes.each { |field, values| log_field_change(field, *values) }
     end
 
-    def log_field_change(field, old_value, new_value, author)
+    def log_field_change(field, old_value, new_value)
       body = "changed #{field} from #{old_value} to #{new_value}"
-      self.log_entries.create(author_data.merge(body: body))
+      log_receiver.log_entries.create(author_data.merge(body: body))
     end
 
     def author_data
-      data = Hash(author).with_indifferent_access
+      data = Hash(log_receiver.author).with_indifferent_access
       {
-        author_id:   data[:id],
-        author_type: data[:type],
-        author_name: data[:name]
+          author_id:   data[:id],
+          author_type: data[:type],
+          author_name: data[:name]
       }
     end
   end
