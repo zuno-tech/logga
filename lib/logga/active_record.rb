e38a6c0b6b862faa30f10eaf35f522c5bddbdccb
Lorenzo Tello <ltello@zonedigital.com>
2017-08-05 09:10:51 +0100

    extend logga to model deletions

--- lib/logga/active_record.rb
+++ lib/logga/active_record.rb
@@ -2,7 +2,7 @@ module Logga
   module ActiveRecord
     extend ActiveSupport::Concern
 
-    EXCLUDED_KEYS          = [:created_at, :updated_at, :log, :sent_photos_chaser_email, :sent_after_sales_emails]
+    EXCLUDED_KEYS          = [:created_at, :deleted_at, :updated_at, :log, :sent_photos_chaser_email, :sent_after_sales_emails]
     EXCLUDED_KEYS_SUFFIXES = [:_id, :_filenames]
 
     included do
@@ -12,8 +12,9 @@ module Logga
 
     class_methods do
       def add_log_entries_for(*actions, to: :self, fields: {})
-        after_create :log_model_creation if actions.include?(:create)
-        after_update :log_model_changes  if actions.include?(:update)
+        after_create  :log_model_creation if actions.include?(:create)
+        after_destroy :log_model_deletion if actions.include?(:delete)
+        after_update  :log_model_changes  if actions.include?(:update)
         define_method(:log_receiver) { to == :self ? self : send(to) }
         self.log_fields = fields
       end
@@ -22,6 +23,12 @@ module Logga
     def log_model_creation
       body_generator = ->(record) { default_creation_log_body(record) }
       body = log_fields.fetch(:created_at, body_generator).call(self)
+      log_receiver.log_entries.create(author_data.merge(body: body))
+    end
+
+    def log_model_deletion
+      body_generator = ->(record) {default_deletion_log_body(record)}
+      body           = log_fields.fetch(:deleted_at, body_generator).call(self)
       log_receiver.log_entries.create(author_data.merge(body: body))
     end
 
@@ -61,6 +68,13 @@ module Logga
 
     def default_change_log_body(record, field, old_value, new_value)
       "#{record.class.name.demodulize.titleize} #{field} set to #{new_value}"
+    end
+
+    def default_deletion_log_body(record)
+      [
+        "#{record.class.name.demodulize} removed",
+        ("(#{record.name})" if record.try(:name))
+      ].compact.join(' ')
     end
   end
 end
