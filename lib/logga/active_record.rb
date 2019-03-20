331f2b1ee0d904090e30371adcbada713587f5ff
Stuart <stuart.chinery@gmail.com>
2019-03-20 15:18:22 +0000

    Not excluding log_fields if they have been asked for, regardless of the default excludes

--- lib/logga/active_record.rb
+++ lib/logga/active_record.rb
@@ -37,9 +37,10 @@ module Logga
 
     def log_model_changes
       field_changes = previous_changes.reject do |k, _|
-        excluded_fields.include?(k.to_sym) ||
-        EXCLUDED_KEYS.include?(k.to_sym)   ||
-        EXCLUDED_KEYS_SUFFIXES.any? { |suffix| k.to_s.end_with?(suffix.to_s) }
+        !log_fields.include?(k.to_sym) &&
+        (excluded_fields.include?(k.to_sym) ||
+         EXCLUDED_KEYS.include?(k.to_sym)   ||
+         EXCLUDED_KEYS_SUFFIXES.any? { |suffix| k.to_s.end_with?(suffix.to_s) })
       end
       log_field_changes(field_changes)
     end
